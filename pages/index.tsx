e4d2ef3622fa55f2754509e432c6407b0ff415f7
Pablo Moreno <ssm.pablo@gmail.com>
2022-08-21 17:13:32 +0200

    tailwindcss installation

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/layout/Layout';
 const Home: NextPage = () => {
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <h1 className='text-3xl font-bold bg-red-500'>Body (tailwinds is installed)</h1>
     </Layout>
   )
 }
